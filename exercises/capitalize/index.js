df4803617c6a63fe2aa74c4dfbd26f13249cb9b9
Yawen Qian <qian91328@gmail.com>
2020-04-02 01:23:12 -0500

    Add solutions for some string exercises

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,14 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const words = [];
+
+    for (let s of str.split(' ')) {
+        words.push(s[0].toUpperCase() + s.slice(1));
+    }
+
+    return words.join(' ');
+}
 
 module.exports = capitalize;
