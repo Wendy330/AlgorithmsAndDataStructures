39b1f40ff815a7aef6343aeaa5894f5c9a191690
Yawen Qian <qian91328@gmail.com>
2020-04-03 00:31:00 -0500

    Soltions of runtime complexity

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,40 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// To improve the speed of recursive solution
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    }
+}
+
+// Recursive solution
+function slowFib(n) {
+    if(n < 2) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
+
+// function fib(n) {
+//     const numlist = [0, 1];
+//
+//     for (let i = 0; i < n; i++) {
+//         numlist.push(numlist[i] + numlist[i+1]);
+//     }
+//
+//     return numlist[n];
+//
+// }
 
 module.exports = fib;
